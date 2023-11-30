d2301e26d59a2b7780047199e7a23746eb3aea11
Iris-Fla <irith.fla@gmail.com>
2023-11-30 19:01:07 +0900

    ✔1130

--- composables/Count.ts
+++ composables/Count.ts
@@ -1,36 +1,14 @@
 export const PlayerData = () => {
     return useState('Pdata', () => shallowRef({
         Datalist:[
-            {id:1, title:'にゃおーん',weight:1,},
-            {id:2, title:'にゃおーん',weight:2,},
-            {id:3, title:'にゃおーん',weight:3,},
-            {id:4, title:'にゃおーん',weight:4,},
-            {id:5, title:'にゃおーん',weight:5,},
-            {id:6, title:'にゃおーん',weight:1,},
-            {id:7, title:'にゃおーん',weight:2,},
-            {id:8, title:'にゃおーん',weight:3,},
-            {id:9, title:'にゃおーん',weight:4,},
-            {id:10, title:'にゃおーん',weight:5,},
+            {id:0, title:'にゃおーん',weight:1,},
+            {id:1, title:'にゃおん',weight:2,},
+            {id:2, title:'にゃお',weight:3,},
         ]
     }))
 }
 export const TitleData = () => {
     return useState('Tdata', () => "ふにゃ～")
-}
-
-export const WeightData = () => {
-    return useState('Wdata', () => shallowRef({
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-        ten: 10,
-    }))
 }
 
 export const BiasData = () => {
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { PlayerData,TitleData,BiasData } from '@/composables/Count'
+import { PlayerData, TitleData, BiasData } from '@/composables/Count'
 const Pdata = PlayerData()
 const Tdata = TitleData()
+
+const DeleteData = (DD:number) => {
+
+}
 
 </script>
 <template>
@@ -9,6 +13,24 @@ const Tdata = TitleData()
         <h1>にゃーん...</h1>
         <h2>{{ Tdata }}</h2>
         <h2>{{ Pdata.Datalist[1].title }}</h2>
-        <CountMain />
+        <Alert theme="primary" v-for="list in Pdata.Datalist" :key="list.id">
+            <Row align-items="center">
+                <Col col="10">
+                <BInputGroup>
+                    <BInputGroupText id="BInputGroup-sizing-lg">
+                        タイトル
+                    </BInputGroupText>
+                    <BFormInput type="text" placeholder="タイトルを入力" aria-label="Title" v-model="list.title"/>
+                    <BInputGroupText>重み</BInputGroupText>
+                    <BFormInput type="text" placeholder="重みを入力" aria-label="Weight" v-model="list.weight"/>
+                </BInputGroup>
+                </Col>
+                <Col col="2">
+                <b-button button="primary" @click="DeleteData(list.id)">
+                    削除
+                </b-button>
+                </Col>
+            </Row>
+        </Alert>
     </Container>
 </template>
