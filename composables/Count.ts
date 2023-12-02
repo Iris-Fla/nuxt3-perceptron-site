15fa600133f00fa7928125608200153be8b83119
Iris-fla <Irith.fla@gmail.com>
2023-12-02 14:46:37 +0900

    🔧1202更新
    keyの仕様を使ってページの再描画を行いたい...

--- composables/Count.ts
+++ composables/Count.ts
@@ -1,9 +1,9 @@
 export const PlayerData = () => {
     return useState('Pdata', () => shallowRef({
         Datalist:[
-            {id:0, title:'にゃおーん',weight:1,},
-            {id:1, title:'にゃおん',weight:2,},
-            {id:2, title:'にゃお',weight:3,},
+            {title:'にゃおーん',weight:1,},
+            {title:'にゃおん',weight:2,},
+            {title:'にゃお',weight:3,},
         ]
     }))
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,51 @@
 <script setup lang="ts">
-import { PlayerData, TitleData, BiasData } from '@/composables/Count'
-const Pdata = PlayerData()
-const Tdata = TitleData()
+import { PlayerData, TitleData, BiasData } from "@/composables/Count";
+const Pdata = PlayerData();
+const Tdata = TitleData();
+const Bdata = BiasData();
 
-const DeleteData = (DD:number) => {
-
-}
+const DeleteTaskData = (title: string, weight: number) => {
+  Pdata.value.Datalist = Pdata.value.Datalist.filter(
+    (item) => item.title !== title || item.weight !== weight
+  );
+};
 
 </script>
 <template>
-    <Container>
-        <h1>にゃーん...</h1>
-        <h2>{{ Tdata }}</h2>
-        <h2>{{ Pdata.Datalist[1].title }}</h2>
-        <Alert theme="primary" v-for="list in Pdata.Datalist" :key="list.id">
-            <Row align-items="center">
-                <Col col="10">
-                <BInputGroup>
-                    <BInputGroupText id="BInputGroup-sizing-lg">
-                        タイトル
-                    </BInputGroupText>
-                    <BFormInput type="text" placeholder="タイトルを入力" aria-label="Title" v-model="list.title"/>
-                    <BInputGroupText>重み</BInputGroupText>
-                    <BFormInput type="text" placeholder="重みを入力" aria-label="Weight" v-model="list.weight"/>
-                </BInputGroup>
-                </Col>
-                <Col col="2">
-                <b-button button="primary" @click="DeleteData(list.id)">
-                    削除
-                </b-button>
-                </Col>
-            </Row>
-        </Alert>
-    </Container>
+  <Container>
+    <h1>にゃーん...お</h1>
+    <h2>{{ Tdata }}</h2>
+    <Alert theme="primary" v-for="list in Pdata.Datalist" :key="list.title">
+      <Row align-items="center">
+        <Col col="11">
+          <BInputGroup>
+            <BInputGroupText id="BInputGroup-sizing-lg">
+              タイトル
+            </BInputGroupText>
+            <BFormInput
+              type="text"
+              placeholder="タイトルを入力"
+              aria-label="Title"
+              v-model="list.title"
+            />
+            <BInputGroupText>重み</BInputGroupText>
+            <BFormInput
+              type="text"
+              placeholder="重みを入力"
+              aria-label="Weight"
+              v-model="list.weight"
+            />
+          </BInputGroup>
+        </Col>
+        <Col col="1">
+          <b-button
+            button="primary"
+            @click="DeleteTaskData(list.title, list.weight)"
+          >
+            削除
+          </b-button>
+        </Col>
+      </Row>
+    </Alert>
+  </Container>
 </template>
