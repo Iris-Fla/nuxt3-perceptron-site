2861968dbce548e393f8dd84ae4ee7006cab159f
Iris-Fla <irith.fla@gmail.com>
2023-12-20 14:50:28 +0900

    [Add]計算ページ

--- pages/index.vue
+++ pages/index.vue
@@ -26,10 +26,7 @@ const MakeTaskData = () => {
   }
 };
 
-const playQuestion = () => {
-  const question = userData.value.dataList;
-
-};
+const playQuestion = () => {};
 
 // 現在のIDの最大値を取得
 const maxId = () => {
@@ -44,39 +41,80 @@ const maxId = () => {
 
 const showData = () => {
   console.log(userData.value.dataList);
-}
-
-
+};
 </script>
 <template>
   <Container>
     <h2>{{ userData.titleData }}</h2>
-    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11" v-if="alertMessage">
-      <useToast/>
+    <div
+      class="position-fixed bottom-0 end-0 p-3"
+      style="z-index: 11"
+      v-if="alertMessage"
+    >
+      <useToast />
     </div>
     <Alert shadow theme="light">
-        <BInputGroup>
-          <BInputGroupText id="BInputGroup
-            ">
+      <BInputGroup>
+        <BInputGroupText
+          id="BInputGroup
+            "
+          class="d-none d-md-block"
+        >
+          タイトル
+        </BInputGroupText>
+        <BFormInput
+          type="text"
+          placeholder="タイトルを入力"
+          aria-label="Title"
+          v-model="userData.titleData"
+        />
+        <BInputGroupText class="d-none d-md-block">バイアス</BInputGroupText>
+        <BFormInput
+          type="text"
+          placeholder="バイアスを入力"
+          aria-label="Weight"
+          v-model="userData.biasData"
+        />
+        <b-button button="success" @click="MakeTaskData()"
+          ><BIcon icon="bi:plus-circle"></BIcon
+        ></b-button>
+      </BInputGroup>
+    </Alert>
+    <TransitionGroup name="fade" tag="div">
+      <div v-for="list in userData.dataList" :key="list.id">
+        <BInputGroup class="inputGroup">
+          <BInputGroupText id="BInputGroup" class="d-none d-md-block">
             タイトル
           </BInputGroupText>
-          <BFormInput type="text" placeholder="タイトルを入力" aria-label="Title" v-model="userData.titleData" />
-          <BInputGroupText>バイアス</BInputGroupText>
-          <BFormInput type="text" placeholder="バイアスを入力" aria-label="Weight" v-model="userData.biasData" />
-          <b-button button="success" @click="MakeTaskData()"><BIcon icon="bi:plus-circle"></BIcon></b-button>
+          <BFormInput
+            type="text"
+            placeholder="タイトルを入力"
+            aria-label="Title"
+            v-model="list.id"
+          />
+          <BInputGroupText class="d-none d-md-block">重み</BInputGroupText>
+          <BFormInput
+            type="text"
+            placeholder="重みを入力"
+            aria-label="Weight"
+            v-model="list.weight"
+          />
+          <b-button button="danger" @click="DeleteTaskData(list.id)">
+            消 <BIcon icon="bi:dash-circle" />
+          </b-button>
         </BInputGroup>
-    </Alert>
-    <transition-group name="fade">
+      </div>
+    </TransitionGroup>
+    <!-- <transition-group name="fade">
       <Alert margin="s-5 e-5" class="alert-style" shadow theme="primary" v-for="list in userData.dataList" :key="list.id">
         <Row align-items="center">
-          <Col col="11">
+          <Col col="10">
           <BInputGroup>
-            <BInputGroupText id="BInputGroup
-            ">
-              タイトル
+            <BInputGroupText id="BInputGroup" class="d-none d-md-block">
+            タイトル
             </BInputGroupText>
             <BFormInput type="text" placeholder="タイトルを入力" aria-label="Title" v-model="list.id" />
-            <BInputGroupText>重み</BInputGroupText>
+            <BInputGroupText class="d-none d-md-block">重み</BInputGroupText>
             <BFormInput type="text" placeholder="重みを入力" aria-label="Weight" v-model="list.weight" />
           </BInputGroup>
           </Col>
@@ -87,18 +125,23 @@ const showData = () => {
           </Col>
         </Row>
       </Alert>
-    </transition-group>
+    </transition-group> -->
     <b-button button="primary" @click="showData">実行す</b-button>
     <h1>たまざらし</h1>
   </Container>
 </template>
 
 <style scoped>
-.alert-style {
- border-radius: 20px;
- background-color: #4EA6D7;
+.inputGroup {
+    padding: 20px;
+    border: 10px;
+    border-color: #4ea6d7;
+    background-color: aqua;
 }
-
+.alert-style {
+  border-radius: 20px;
+  background-color: #4ea6d7;
+}
 
 .fade-move,
 .fade-enter-active,
@@ -109,7 +152,7 @@ const showData = () => {
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-  transform: translate(-50px,0px)
+  transform: translate(-50px, 0px);
 }
 
 .fade-leave-active {
---
+++ pages/question.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const userData = useData();
+</script>
+<template>
+  <Card>
+    <div v-for="items in userData.dataList" :key="items.id">
+      <CardHeader>{{ userData.titleData }}</CardHeader>
+      <CardText>{{ items.title }}</CardText>
+    </div>
+  </Card>
+</template>
